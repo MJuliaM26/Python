b654dd9b76ced75dcb1f2eea3fa6aeef704fe03b
MichałowskaJulia <95963847+MJuliaM26@users.noreply.github.com>
2022-01-14 21:51:58 +0100

    Add files via upload

---
+++ zajj.py
@@ -0,0 +1,37 @@
+
+
+#Zadanie 2
+#print("Zadanie 2, a")
+#for i in range(0, 1):
+#    for j in range(1, 2):
+#        print(j * "*")
+#    for j in range(1, 3):
+#     print(j * "*")
+#    for j in range(1, 4):
+#        print(j * "*")
+     
+     
+     
+#Zadanie 2
+#print("Zadanie 2, b")
+#for a in range (0, 1):
+#    for x in range(1, 3):
+#        print (x * "*")
+#    for x in range(1, 4):
+#        print(x * "*")
+#    for x in range(1, 5):
+#       print(x * "*")
+#    for x in range(1, 6):
+#        print(x * "*")
+
+#z1
+#number = int(input('number: '))
+
+#if number < 1: 
+#    print("True")
+#else:
+#    print("False")
+
+#lista = [12, 5, 1, 64, 3, 1, 56, 3, 1]
+#najmniejsza = None
+#for i in lista:
